c979694db7495de110c0004e1958e1a985bab0f4
zeyadsleem <zeyadsleem1@gmail.com>
2024-01-18 23:34:02 +0200

    init the project

---
+++ src/components/ListItem.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+
+defineProps<{ isChecked?: boolean | false }>()
+
+</script>
+
+<template>
+  <label :class="{ 'checked': isChecked }">
+    <input type="checkbox" :checked="isChecked" />
+    <slot></slot>
+  </label>
+</template>
+
+<style scoped>
+label {
+  cursor: pointer;
+}
+
+.checked {
+  text-decoration: line-through;
+}
+</style>
